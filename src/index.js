d373f4ef6cd0cdda359f79c1fda2e5e3d81779ef
rozabelay <rozaybeyn.belay@gmail.com>
2024-11-29 14:29:55 +0100

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const helmet = require('helmet');
 const cors = require('cors');
-const port = 3000
+const PORT = 3000
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const todoRoutes = require('./routes/todoRoutes');
+app.use('/api/todos', todoRoutes);
 
 app.get("/",(req, res, next)=>{
     res.send("To do list Home page");
 })
 
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`)
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
   })
+
